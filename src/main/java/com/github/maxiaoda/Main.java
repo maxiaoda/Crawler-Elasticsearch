dd55b6fd4f456fa540717c23d1063af5372a47ca
maxiaoda <631677979@qq.com>
2020-03-10 22:38:35 +0800

    Update:使用jdbc进行爬虫，爬取到的网址、标题、内容添加到数据库中。

--- src/main/java/com/github/maxiaoda/Main.java
+++ src/main/java/com/github/maxiaoda/Main.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final String USER_NAME = "root";
@@ -23,37 +24,38 @@ public class Main {
     @SuppressFBWarnings
     public static void main(String[] args) throws IOException, SQLException {
         Connection connection = DriverManager.getConnection("jdbc:h2:file:C:\\Users\\abc\\IdeaProjects\\Crawler-Elasticsearch\\news", USER_NAME, PASSWORD);
-        while (true) {
+        String link = null;
 
-            List<String> linkPool = loadUrlsFromDatabase(connection, "select LINK from LINKS_TO_BE_PROCESSED");
-
-            if (linkPool.isEmpty()) {
-                break;
-            }
-
-            //remove删除并返回值，ArrayList从尾部删除更有效。
-            String link = linkPool.remove(linkPool.size() - 1);
-            insertLinkIntoDatabase(connection, link, "delete from LINKS_TO_BE_PROCESSED where link = ?");
-
+        //先从数据库里拿出来一个链接，（拿出来并从数据库中删除）
+        while ((link = getNextLinkThenDelete(connection)) != null) {
             if (isLinkProcessed(connection, link) || link.contains("https:\\/\\/")) {
                 //符合条件的的链接，返回
                 continue;
             }
-
             if (isNewsLink(link)) {
                 Document doc = httpGetAndParseHtml(link);
                 parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
-                storeIntoDataOfNewsPage(link, doc);
-                insertLinkIntoDatabase(connection, link, "insert into LINKS_ALREADY_PROCESSED(LINK) values (?)");
+                storeIntoDataOfNewsPage(connection, link, doc);
+                updateDatabase(connection, link, "insert into LINKS_ALREADY_PROCESSED(LINK) values (?)");
             }
         }
+    }
+
+    private static String getNextLinkThenDelete(Connection connection) throws SQLException {
+        String link = getNextLink(connection, "select LINK from LINKS_TO_BE_PROCESSED limit 1");
+        if (link != null) {
+            updateDatabase(connection, link, "delete from LINKS_TO_BE_PROCESSED where link = ?");
+        }
+        return link;
     }
 
     //将<a>里的<href>加入到待处理的链接池（linkPool）
     private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
         for (Element aTagLink : doc.select("a")) {
             String href = aTagLink.attr("href");
-            insertLinkIntoDatabase(connection, href, "insert into LINKS_TO_BE_PROCESSED(LINK) values (?)");
+            if (!href.toLowerCase().startsWith("javascript")) {
+                updateDatabase(connection, href, "insert into LINKS_TO_BE_PROCESSED(LINK) values (?)");
+            }
         }
     }
 
@@ -74,7 +76,7 @@ public class Main {
     }
 
     //每次处理后更新数据库,从待处理池中取一个；处理完后从池中删除，包括数据库。
-    private static void insertLinkIntoDatabase(Connection connection, String link, String sql) throws SQLException {
+    private static void updateDatabase(Connection connection, String link, String sql) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, link);
             statement.executeUpdate();
@@ -82,35 +84,43 @@ public class Main {
     }
 
     //待处理的链接池,从数据库加载即将处理的链接
-    private static List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
+    @SuppressFBWarnings
+    private static String getNextLink(Connection connection, String sql) throws SQLException {
         List<String> results = new ArrayList<>();
         ResultSet resultSet = null;
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                results.add(resultSet.getString(1));
+                return resultSet.getString(1);
             }
         } finally {
             if (resultSet != null) {
                 resultSet.close();
             }
         }
-        return results;
+        return null;
     }
 
     //储存新闻页面的数据，如果不是就什么也不做
-    private static void storeIntoDataOfNewsPage(String link, Document doc) {
+    private static void storeIntoDataOfNewsPage(Connection connection, String link, Document doc) throws SQLException {
         ArrayList<Element> articleTags = doc.select("article");
 
         if (!articleTags.isEmpty()) {
             for (Element articleTag : articleTags) {
                 String title = articleTags.get(0).child(0).text();
+                String content = articleTag.select("p").stream().map(Element::text).collect(Collectors.joining("\n"));
+
                 System.out.println(link);
                 System.out.println(title);
 
+                try (PreparedStatement statement = connection.prepareStatement("insert into NEWS ( url, title, content, created_at, modified_at) values (?,?,?,noW(),now())")) {
+                    statement.setString(1, link);
+                    statement.setString(2, title);
+                    statement.setString(3, content);
+                    statement.executeUpdate();
 
-//                //文章内容
-//               articleTags.get(0).child(5).select("p");
+                }
+
             }
         }
     }
@@ -126,10 +136,7 @@ public class Main {
         httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36");
 
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-
-            //System.out.println(response1.getStatusLine());
             HttpEntity entity1 = response1.getEntity();
-
             String html = EntityUtils.toString(entity1);
             return Jsoup.parse(html);
         }
@@ -142,7 +149,7 @@ public class Main {
     }
 
     private static boolean isSinaLink(String link) {
-        return "https://sina.cn".equals(link);
+        return "https://news.sina.cn".equals(link);
     }
 
     private static boolean isNewsPage(String link) {
---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,16 @@
+create table LINKS_TO_BE_PROCESSED(
+link varchar(2000)
+);
+
+create table LINKS_ALREADY_PROCESSED(
+link varchar(2000)
+);
+
+create table NEWS(
+ID    bigint primary key auto_increment,
+TITLE text,
+CONTENT text,
+URL varchar(2000),
+CREATED_AT timestamp,
+MODIFIED_AT timestamp
+);
